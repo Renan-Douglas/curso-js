7c41b43d60cbc66875c2600172c02f949a794ad4
Renan-Douglas <renandouglasfb12@gmail.com>
2022-10-29 14:18:42 -0300

    aula 11 condicoes pt1

---
+++ aula11/ex008.js
@@ -0,0 +1,11 @@
+var vel = 20
+
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+
+    if (vel > 40){
+
+        console.log('Voce ultrapassou a velocidade permitida. Multado!')
+
+    }
+    
+console.log('Dirija sempre com sinto de seguranca')
