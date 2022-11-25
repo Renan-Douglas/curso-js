95b5910d7693f770aba9d8edc014f61396a5263c
Renan-Douglas <renandouglasfb12@gmail.com>
2022-11-25 17:44:13 -0300

    termino exercicio part 8

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -35,13 +35,80 @@ function adicionar() {
 
     if (isNumero(num.value) && !inLista(num.value, valores)) {
 
-        
+        valores.push(Number(num.value))
+
+        let item = document.createElement('option')
+
+        item.text = `Valor ${num.value} adicionado.`
+
+        lista.appendChild(item)
+
+        res.innerHTML = ''
+
+    } else  {
+
+        if (num.value > 100) {
+
+            alert('Número acima do valor máximo')
+
+        } else if (num.value < 1) {
+
+            alert('Número abaixo do valor mínimo')
+
+        } else {
+
+            alert('Valor invalido ou já encontrado na lista')
+
+        }
+
+    }
+
+    num.value = ''
+
+    num.focus()
+
+}
+
+function finalizar() {
+
+    if (valores.length == 0) {
+
+        alert('Erro, vetor vazio. Adicione valores para prosseguir')
 
     } else {
 
-        alert('Valor invalido ou já encontrado na lista')
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+
+        for (let pos in valores) {
+
+            soma += valores[pos]
+
+            if (valores[pos] > maior) {
+
+                maior = valores[pos]
+
+            } if (valores[pos] < menor) {
+
+                menor = valores[pos]
+
+            }
+
+        }
+
+        media = soma / tot
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} número cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado é o ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado é o ${menor}.</p>`
+        res.innerHTML += `Somando todos os valores o resultado é ${soma}`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}</p>`
+
 
     }
 
 }
-
